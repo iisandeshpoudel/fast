fe3a6a9333fb51c971e4f9ef871a4886e88515c3
ThinZin <51796069+iisandeshpoudel@users.noreply.github.com>
2024-11-05 02:36:52 +0545

    Update Testimonials.tsx

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -3,22 +3,22 @@ import { MessageSquare } from 'lucide-react';
 
 const testimonials = [
   {
-    name: "Sarah Johnson",
+    name: "Apekshya Shah",
     role: "CEO, TechVision",
-    content: "Bichitras transformed our digital presence completely. Their expertise in both design and development is unmatched.",
-    image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?auto=format&fit=crop&w=200&q=80"
+    content: "In Nepal has Bichitras transformed our digital presence completely. Their expertise in both design and development is unmatched.",
+    image: "https://nepaleconomicforum.org/wp-content/uploads/2021/11/Apekshya-Shah_headshot-e1623095590109-1.jpg"
   },
   {
-    name: "Michael Chen",
+    name: "Amigo Khadka",
     role: "Marketing Director, InnovateCo",
     content: "The level of creativity and technical excellence they bring to every project is extraordinary. Truly a game-changer for our business.",
-    image: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?auto=format&fit=crop&w=200&q=80"
+    image: "https://media.licdn.com/dms/image/v2/C4D03AQFkz0DTMja7Jw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1647852347863?e=1736380800&v=beta&t=MP5VqP8FBZ8Xv8wN4tScDwEh5k-Rhrkch0JJw5NE0jw"
   },
   {
-    name: "Emma Williams",
+    name: "Manish Ghimire",
     role: "Founder, DesignHub",
     content: "Working with Bichitras has been an incredible experience. They don't just deliver projects; they deliver excellence.",
-    image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?auto=format&fit=crop&w=200&q=80"
+    image: "https://media.licdn.com/dms/image/v2/D4D03AQFMQwIT-Gau8g/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1724697353314?e=1736380800&v=beta&t=5no7yNnoMA3jfvicxT7n4jd_4B2I4A3t46XQMCgdXdI"
   }
 ];
 
